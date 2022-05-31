550fd8a3780432399c17f7b888658ab9aba1d1ce
Jose Manuel Sanchez <99229719+DaddyLipi@users.noreply.github.com>
2022-05-31 11:32:37 -0400

    Error, resta 10, 15 no da 5

--- src/resta.test.js
+++ src/resta.test.js
@@ -1,8 +1,11 @@
-import restar from "./sumador.js";
+import restar from "./resta.js";
 
 describe("Prueba ventas", () => {
   it("El monto efectivo deberia restar el monto efectivo (0,0)", () => {
     expect(restar(0, 0)).toEqual(0);
+  });
+  it("El monto efectivo deberia restar el monto efectivo (10,5)", () => {
+    expect(restar(10, 15)).toEqual(5);
   });
 });
 //comentario para probar el npm install y git
