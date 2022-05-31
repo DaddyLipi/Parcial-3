a9073db2a5c9339da31e980c79e5f84bff91ac0a
Jose Manuel Sanchez <99229719+DaddyLipi@users.noreply.github.com>
2022-05-31 11:58:09 -0400

    Prueba monedas 2, rojo

--- src/MonedasCambio.test.js
+++ src/MonedasCambio.test.js
@@ -3,5 +3,8 @@ describe("Prueba monedas", () => {
   it("Si tengo 0 de cambio las monedas deberian salir 0", () => {
     expect(Cambio(0)).toEqual(0);
   });
+  it("Si tengo 1 de cambio las monedas deberian salir 1", () => {
+    expect(Cambio(1)).toEqual(1);
+  });
 });
 //com
